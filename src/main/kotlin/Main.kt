5888a915d3a752f7ddc92a1912485d3fc1954199
blezz-tech <markus.jenya04@yandex.ru>
2025-06-24 22:04:32 +0300

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,21 @@
 package org.example
 
-fun main() {
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val parts = IntArray(3)
+    for (i in 0 until 3) {
+        parts[i] = reader.readLine().toInt()
+    }
+    val sortedParts = parts.sorted()
+    writer.write(sortedParts[1])
+
+    reader.close()
+    writer.close()
 }
